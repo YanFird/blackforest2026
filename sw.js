dff9d4339ef2a67b35ca9414ab57a352b21b87d4
Yaniv F. <yaniv.fridberg@gmail.com>
2026-09-08 15:31:34 +0300

    feat: add Freiburg market transition route

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'blackforest-2026-v20260907-waldshut-open-air-nav';
+const CACHE_NAME = 'blackforest-2026-v20260908-freiburg-market-transfer';
 const CORE_ASSETS = [
   './',
   './index.html',
